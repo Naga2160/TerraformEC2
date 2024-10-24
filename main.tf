2765555943a180543ae2f193f98d5d7dc1056920
Naga2160 <reddykasi46@gmail.com>
2024-10-24 12:46:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ region="ap-south-1"
 resource "aws_instance" "one"{
 count=4
 ami="ami-0e0e417dfa2028266"
-instance_type="t2.micro"
+instance_type="t2.medium"
 key_name = "dev4pm"
 vpc_security_group_ids = ["sg-049e104c5f8955ef5"]
 tags={
